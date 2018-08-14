ca741f45c3f63c48b4f99faa9e2b18e9fcea610b
Juan Jose <juanjotoshi2@gmail.com>
2018-08-14 01:01:14 -0500

    subiendo plantilla de api

---
+++ prestamosAPI/src/main/java/com/prestamo/PrestamosApiApplication.java
@@ -0,0 +1,12 @@
+package com.prestamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamosApiApplication.class, args);
+	}
+}
---
+++ prestamosAPI/src/test/java/com/prestamo/PrestamosApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prestamo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrestamosApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
